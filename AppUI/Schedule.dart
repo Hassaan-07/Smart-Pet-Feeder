d39c4afa9140814e00c1de78e581fc1124b74e73
KarimShoukatAli <106752903+KarimShoukatAli@users.noreply.github.com>
2023-12-09 19:38:20 +0500

    Update Schedule.dart

--- AppUI/Schedule.dart
+++ AppUI/Schedule.dart
@@ -1,3 +1,17 @@
+import '/flutter_flow/flutter_flow_icon_button.dart';
+import '/flutter_flow/flutter_flow_theme.dart';
+import '/flutter_flow/flutter_flow_util.dart';
+import '/flutter_flow/flutter_flow_widgets.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+
+import 'schedule_copy_model.dart';
+export 'schedule_copy_model.dart';
+
 Container(
   width:  double.infinity,
   height:  852*fem,
