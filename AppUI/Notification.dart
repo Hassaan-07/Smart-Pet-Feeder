8fd20c755f58595aa093db560b8b5fa5d237d9cc
KarimShoukatAli <106752903+KarimShoukatAli@users.noreply.github.com>
2023-12-09 19:37:44 +0500

    Update Notification.dart

--- AppUI/Notification.dart
+++ AppUI/Notification.dart
@@ -1,3 +1,17 @@
+import '/flutter_flow/flutter_flow_icon_button.dart';
+import '/flutter_flow/flutter_flow_theme.dart';
+import '/flutter_flow/flutter_flow_util.dart';
+import '/flutter_flow/flutter_flow_widgets.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+
+import 'notifications_copy_model.dart';
+export 'notifcations_copy_model.dart';
+
 Container(
   // notifications3ch (59:125)
   width:  double.infinity,
